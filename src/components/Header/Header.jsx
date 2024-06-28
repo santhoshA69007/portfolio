01fe4ec50a1f3bbdad538068917da4f24c1ca21d
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-06-29 00:37:25 +0530

    edited

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+import styles from './Header.module.css'
+
+function Header() {
+    return (
+        <div className={styles.image}>
+           <img src={"./images/earth-moon-4k-ri.jpg"} alt='earth photos'/>
+        </div>
+    )
+}
+
+export default Header
